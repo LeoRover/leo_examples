ed0f0b3a8fd786b18febcd7b60b77c6790cbc9f2
Błażej Sowa <bsowa123@gmail.com>
2021-08-11 16:36:19 +0200

    Put the code into a class

--- scripts/follow_ar_tag.py
+++ scripts/follow_ar_tag.py
@@ -4,47 +4,43 @@ import math
 
 import rospy
 
-from geometry_msgs.msg import Twist, Vector3
+from geometry_msgs.msg import Twist, TwistStamped, Vector3
 from ar_track_alvar_msgs.msg import AlvarMarkers
 
-rospy.init_node("follow_ar_tag")
 
-follow_id = rospy.get_param("~follow_id", 0)
-marker_timeout = rospy.Duration(rospy.get_param("~marker_timeout", 0.5))
-min_ang_vel = rospy.get_param("~min_ang_vel", 0.1)
-max_ang_vel = rospy.get_param("~max_ang_vel", 1.0)
-angle_min = rospy.get_param("~angle_min", 0.1)
-angle_max = rospy.get_param("~angle_max", 0.7)
+class ARTagFollower:
+    def __init__(self):
+        self.last_ts = rospy.Time()
+        self.last_position = Vector3()
+        self.twist_cmd = Twist()
 
-last_ts = rospy.Time()
-last_position = Vector3()
-twist_cmd = Twist()
+        self.get_parameters()
 
+        self.cmd_vel_pub = rospy.Publisher("cmd_vel", Twist, queue_size=1)
 
-def ar_pose_callback(msg):
-    global last_ts, last_position
-    for marker in msg.markers:
-        if marker.id != follow_id:
-            continue
-        if marker.header.stamp < last_ts:
-            rospy.logwarn_throttle(3.0, "Got marker position with an older timestamp")
-            continue
+        self.ar_pose_sub = rospy.Subscriber(
+            "ar_pose_marker", AlvarMarkers, self.callback_ar_pose
+        )
+        self.wheel_odom_sub = rospy.Subscriber(
+            "wheel_odom", TwistStamped, self.callback_wheel_odom
+        )
 
-        last_ts = marker.header.stamp
-        last_position = marker.pose.pose.position
+    def run(self):
+        rate = rospy.Rate(10)
+        while not rospy.is_shutdown():
+            self.update_cmd()
+            self.cmd_vel_pub.publish(self.twist_cmd)
+            rate.sleep()
 
+    def update_cmd(self):
+        # Check for a timeout
+        if self.last_ts + self.marker_timeout < rospy.get_rostime():
+            self.twist_cmd.linear.x = 0.0
+            self.twist_cmd.angular.z = 0.0
+            return
 
-cmd_vel_pub = rospy.Publisher("cmd_vel", Twist, queue_size=1)
-ar_pose_sub = rospy.Subscriber("ar_pose_marker", AlvarMarkers, ar_pose_callback)
-
-rate = rospy.Rate(10)
-while not rospy.is_shutdown():
-    if last_ts + marker_timeout < rospy.get_rostime():
-        twist_cmd.linear.x = 0.0
-        twist_cmd.angular.z = 0.0
-    else:
         # Get the angle to the marker
-        angle = math.atan(last_position.y / last_position.x)
+        angle = math.atan(self.last_position.y / self.last_position.x)
 
         # Check direction
         if angle < 0.0:
@@ -54,16 +50,45 @@ while not rospy.is_shutdown():
             dir = 1.0
 
         # Check angle threshold
-        if angle < angle_min:
+        if angle < self.angle_min:
             ang_cmd = 0.0
         else:
-            if angle > angle_max:
-                angle = angle_max
+            if angle > self.angle_max:
+                angle = self.angle_max
 
-            angle_scaled = (angle - angle_min) / (angle_max - angle_min)
-            ang_cmd = min_ang_vel + angle_scaled * (max_ang_vel - min_ang_vel)
+            angle_scaled = (angle - self.angle_min) / (self.angle_max - self.angle_min)
+            ang_cmd = self.min_ang_vel + angle_scaled * (
+                self.max_ang_vel - self.min_ang_vel
+            )
 
-        twist_cmd.angular.z = dir * ang_cmd
+        self.twist_cmd.angular.z = dir * ang_cmd
 
-    cmd_vel_pub.publish(twist_cmd)
-    rate.sleep()
+    def get_parameters(self):
+        self.follow_id = rospy.get_param("~follow_id", 0)
+        self.marker_timeout = rospy.Duration(rospy.get_param("~marker_timeout", 0.5))
+        self.min_ang_vel = rospy.get_param("~min_ang_vel", 0.1)
+        self.max_ang_vel = rospy.get_param("~max_ang_vel", 1.0)
+        self.angle_min = rospy.get_param("~angle_min", 0.1)
+        self.angle_max = rospy.get_param("~angle_max", 0.7)
+
+    def callback_ar_pose(self, msg):
+        for marker in msg.markers:
+            if marker.id != self.follow_id:
+                continue
+            if marker.header.stamp < self.last_ts:
+                rospy.logwarn_throttle(
+                    3.0, "Got marker position with an older timestamp"
+                )
+                continue
+
+            self.last_ts = marker.header.stamp
+            self.last_position = marker.pose.pose.position
+
+    def callback_wheel_odom(self, msg):
+        pass
+
+
+if __name__ == "__main__":
+    rospy.init_node("follow_ar_tag")
+    ar_tag_follower = ARTagFollower()
+    ar_tag_follower.run()
