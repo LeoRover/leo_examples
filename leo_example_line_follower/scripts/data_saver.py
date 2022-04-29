c99ceb5f2a2d1fe3c244d46579c1a9373c25a97e
Bitterisland6 <2010olo@wp.pl>
2022-04-29 16:28:27 +0200

    leo_example_line_follower: data_saver.py: fixed typos and formatted code with black
    Signed-off-by: Bitterisland6 <2010olo@wp.pl>

--- leo_example_line_follower/scripts/data_saver.py
+++ leo_example_line_follower/scripts/data_saver.py
@@ -12,8 +12,7 @@ import datetime
 from pathlib import Path
 
 
-
-class DataSaver():
+class DataSaver:
     def __init__(self, duration, camera_topic, vel_topic, output_dir):
         self.duration = duration
         self.camera_topic = camera_topic
@@ -21,26 +20,31 @@ class DataSaver():
         self.output_dir = output_dir
 
         self.bridge = cv_bridge.CvBridge()
-        
+
         self.ready = False
-        self.end_time = 0  
+        self.end_time = 0
 
         rospy.loginfo("Making directory for saved images (if it doesn't exist).")
         self.path = "./" + self.output_dir
         Path(self.path).mkdir(parents=True, exist_ok=True)
-        
+
         date = datetime.datetime.now()
         self.img_dir = self.path + "/"
-        self.img_base_name = "%s%s%s%s%s-img" % (date.day, date.month, date.year, date.hour, date.minute)
+        self.img_base_name = "%s%s%s%s%s-img" % (
+            date.day,
+            date.month,
+            date.year,
+            date.hour,
+            date.minute,
+        )
 
-        rospy.loginfo("Opening label file (creating if deons't exist).")
+        rospy.loginfo("Opening label file (creating if doesn't exist).")
         self.label_file = open(self.path + "/" + self.output_dir + "-labels.txt", "a+")
-       
+
         self.counter = 0
-        
+
         self.video_sub = rospy.Subscriber(self.camera_topic, Image, self.data_callback)
         self.vel_sub = rospy.Subscriber(self.vel_topic, Twist, self.vel_callback)
-
 
     def data_callback(self, data: Image):
         if not self.ready:
@@ -56,12 +60,15 @@ class DataSaver():
             cv_img = self.bridge.imgmsg_to_cv2(data, desired_encoding="bgr8")
             img_name = self.img_base_name + str(self.counter) + ".jpg"
 
-            cv2.imwrite(filename=self.img_dir + img_name, img=cv_img, params=[cv2.IMWRITE_JPEG_QUALITY, 100])
+            cv2.imwrite(
+                filename=self.img_dir + img_name,
+                img=cv_img,
+                params=[cv2.IMWRITE_JPEG_QUALITY, 100],
+            )
             self.label_file.write("{}:{}\n".format(img_name, self.label))
             self.counter += 1
-        
 
-    def vel_callback(self, data:Twist):
+    def vel_callback(self, data: Twist):
         if not self.ready:
             self.ready = True
             self.end_time = rospy.Time.now() + rospy.Duration.from_sec(self.duration)
@@ -69,10 +76,43 @@ class DataSaver():
 
 
 def add_arguments(parser: argparse.ArgumentParser):
-    parser.add_argument("duration", default=20.0, type=float, nargs="?", metavar="N", help="duration of recording the data")
-    parser.add_argument("-c", "--cam", default="camera/image_raw", type=str, required=False, dest="camera_topic", help="name of the topic with view from robot")
-    parser.add_argument("-V", "--vel", default="cmd_vel", type=str, required=False, nargs="?", dest="vel_topic", help="name of the velocity topic")
-    parser.add_argument("-o", "--output", default="data", type=str, required=False, nargs="?", dest="output_dir", help="name of an output directory for saved images")
+    parser.add_argument(
+        "duration",
+        default=20.0,
+        type=float,
+        nargs="?",
+        metavar="N",
+        help="duration of recording the data",
+    )
+    parser.add_argument(
+        "-c",
+        "--cam",
+        default="camera/image_raw",
+        type=str,
+        required=False,
+        dest="camera_topic",
+        help="name of the topic with view from robot",
+    )
+    parser.add_argument(
+        "-V",
+        "--vel",
+        default="cmd_vel",
+        type=str,
+        required=False,
+        nargs="?",
+        dest="vel_topic",
+        help="name of the velocity topic",
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        default="data",
+        type=str,
+        required=False,
+        nargs="?",
+        dest="output_dir",
+        help="name of an output directory for saved images",
+    )
 
 
 if __name__ == "__main__":
@@ -80,10 +120,9 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="Record video from rover as images.")
     add_arguments(parser)
-   
+
     rosargv = rospy.myargv(argv=sys.argv)
     args = parser.parse_args(rosargv[1:])
 
     saver = DataSaver(args.duration, args.camera_topic, args.vel_topic, args.output_dir)
     rospy.spin()
-    
